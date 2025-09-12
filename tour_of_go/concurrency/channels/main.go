60525ae268ded2c2ebe241038b84c2d373bfde99
rm-ryou <rmco.420118@gmail.com>
2025-09-13 01:07:09 +0900

    feat: add concurrency

---
+++ tour_of_go/concurrency/channels/main.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+func sum(s []int, c chan int) {
+	sum := 0
+	for _, v := range s {
+		sum += v
+	}
+	c <- sum
+}
+
+func main() {
+	nums := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+	c := make(chan int)
+
+	go sum(nums, c)
+
+	res := <-c
+	fmt.Println(res)
+
+	s := []int{7, 2, 8, -9, 4, 0}
+	go sum(s[:len(s)/2], c)
+	go sum(s[len(s)/2:], c)
+	x, y := <-c, <-c
+
+	fmt.Println(x, y, x+y)
+}
---
+++ tour_of_go/concurrency/sync_mutex/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+type SafeCounter struct {
+	mu sync.Mutex
+	v  map[string]int
+}
+
+func (c *SafeCounter) Inc(key string) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	c.v[key]++
+}
+
+func (c *SafeCounter) Value(key string) int {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	return c.v[key]
+}
+
+func main() {
+	c := SafeCounter{
+		v: make(map[string]int),
+	}
+	for range 1000 {
+		go c.Inc("somekey")
+	}
+
+	time.Sleep(time.Second)
+	fmt.Println(c.Value("somekey"))
+}
