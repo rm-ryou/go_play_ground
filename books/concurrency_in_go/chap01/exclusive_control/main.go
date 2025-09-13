7f2019ba1edc78efbc104911f29a3fffee94637f
rm-ryou <rmco.420118@gmail.com>
2025-09-13 18:32:00 +0900

    feat: 基礎的なコードを追加

---
+++ books/concurrency_in_go/chap01/exclusive_control/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+func main() {
+	var memoryAccess sync.Mutex
+	var data int
+	go func() {
+		memoryAccess.Lock()
+		data++
+		memoryAccess.Unlock()
+	}()
+
+	memoryAccess.Lock()
+	if data == 0 {
+		fmt.Println("The value is 0.")
+	} else {
+		fmt.Printf("The value is %v.\n", data)
+	}
+	memoryAccess.Unlock()
+}
