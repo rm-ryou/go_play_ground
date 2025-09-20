44f1262f4bbfa732a6ddd7a1fa212c555620014b
rm-ryou <rmco.420118@gmail.com>
2025-09-21 02:23:33 +0900

    feat: add concurrency practice

---
+++ books/concurrency_in_go/chap05/heart_beats_of_per_work/main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+)
+
+func doWork(done <-chan any) (<-chan any, <-chan int) {
+	heartbeatCh := make(chan any, 1)
+	workCh := make(chan int)
+	go func() {
+		defer close(heartbeatCh)
+		defer close(workCh)
+
+		for range 10 {
+			select {
+			case heartbeatCh <- struct{}{}:
+			default:
+			}
+
+			select {
+			case <-done:
+				return
+			case workCh <- rand.Intn(10):
+			}
+		}
+	}()
+
+	return heartbeatCh, workCh
+}
+
+func main() {
+	done := make(chan any)
+	defer close(done)
+
+	heartbeat, results := doWork(done)
+	for {
+		select {
+		case _, ok := <-heartbeat:
+			if ok {
+				fmt.Println("pulse")
+			} else {
+				return
+			}
+		case r, ok := <-results:
+			if ok {
+				fmt.Printf("results %v\n", r)
+			} else {
+				return
+			}
+		}
+	}
+}
---
+++ books/concurrency_in_go/chap05/token_bucket/go.mod
@@ -0,0 +1,5 @@
+module example.com
+
+go 1.24.1
+
+require golang.org/x/time v0.13.0 // indirect
