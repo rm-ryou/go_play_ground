5998bf988f1ada04de642afe988855af8f94cf97
rm-ryou <rmco.420118@gmail.com>
2025-11-05 23:14:07 +0900

    wip chap05

---
+++ books/concurrency_in_go/chap05/error_propagation/main.go
@@ -0,0 +1,82 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/exec"
+	"runtime/debug"
+)
+
+type MyError struct {
+	Inner      error
+	Message    string
+	StackTrace string
+	Misc       map[string]any
+}
+
+func wrapError(err error, messagef string, msgArgs ...any) MyError {
+	return MyError{
+		Inner:      err,
+		Message:    fmt.Sprintf(messagef, msgArgs...),
+		StackTrace: string(debug.Stack()),
+		Misc:       make(map[string]any),
+	}
+}
+
+func (err MyError) Error() string {
+	return err.Message
+}
+
+type LowLevelErr struct {
+	error
+}
+
+func isGloballyExec(path string) (bool, error) {
+	info, err := os.Stat(path)
+	if err != nil {
+		return false, LowLevelErr{(wrapError(err, err.Error()))}
+	}
+	return info.Mode().Perm()&0100 == 0100, nil
+}
+
+type IntermediateErr struct {
+	error
+}
+
+func runJob(id string) error {
+	const jobBinPath = "/bad/job/binary"
+	isExecutable, err := isGloballyExec(jobBinPath)
+	if err != nil {
+		return IntermediateErr{wrapError(
+			err,
+			"can't run job %q: requisite binaries not available",
+			id,
+		)}
+	} else if isExecutable == false {
+		return wrapError(
+			nil,
+			"can't run job %q: requisite binaries are not executable",
+			id,
+		)
+	}
+
+	return exec.Command(jobBinPath, "--id="+id).Run()
+}
+
+func handleError(key int, err error, message string) {
+	log.SetPrefix(fmt.Sprintf("[logID: %v]: ", key))
+	log.Printf("%#v", err)
+	fmt.Printf("[%v] %v", key, message)
+}
+
+func main() {
+	err := runJob("1")
+	if err != nil {
+		msg := "There was an unexpected issue; please report this as a bug."
+		if _, ok := err.(IntermediateErr); ok {
+			msg = err.Error()
+		}
+		handleError(1, err, msg)
+	}
+}
