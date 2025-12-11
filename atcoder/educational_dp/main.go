aa170677d85c9889fa9050c10e0d1354fdb6c88b
rm-ryou <rmco.420118@gmail.com>
2025-12-11 23:32:26 +0900

    feat: add vacation

--- atcoder/educational_dp/main.go
+++ atcoder/educational_dp/main.go
@@ -8,6 +8,10 @@ func chMin(x, y int) int {
 	return int(math.Min(float64(x), float64(y)))
 }
 
+func chMax(x, y int) int {
+	return int(math.Max(float64(x), float64(y)))
+}
+
 func abs(x int) int {
 	return int(math.Abs(float64(x)))
 }
@@ -52,4 +56,24 @@ func Flog2(N, K int, height []int) int {
 	}
 
 	return dp[N-1]
+}
+
+func Vacation(N int, a, b, c []int) int {
+	dp := make([][]int, N+1)
+	for i := range dp {
+		dp[i] = make([]int, 3)
+		for j := range dp[i] {
+			dp[i][j] = 0
+		}
+	}
+
+	for i := range N + 1 {
+		if i >= 1 {
+			dp[i][0] = chMax(dp[i-1][1], dp[i-1][2]) + a[i-1]
+			dp[i][1] = chMax(dp[i-1][0], dp[i-1][2]) + b[i-1]
+			dp[i][2] = chMax(dp[i-1][0], dp[i-1][1]) + c[i-1]
+		}
+	}
+
+	return chMax(dp[N][0], chMax(dp[N][1], dp[N][2]))
 }
--- atcoder/educational_dp/main_test.go
+++ atcoder/educational_dp/main_test.go
@@ -77,3 +77,46 @@ func Test_Flog2(t *testing.T) {
 		})
 	}
 }
+
+func Test_Vacation(t *testing.T) {
+	tests := []struct {
+		name    string
+		N       int
+		a, b, c []int
+		want    int
+	}{
+		{
+			name: "return 210",
+			N:    3,
+			a:    []int{10, 20, 30},
+			b:    []int{40, 50, 60},
+			c:    []int{70, 80, 90},
+			want: 210,
+		},
+		{
+			name: "return 100",
+			N:    1,
+			a:    []int{100},
+			b:    []int{10},
+			c:    []int{1},
+			want: 100,
+		},
+		{
+			name: "return 46",
+			N:    7,
+			a:    []int{6, 8, 2, 7, 4, 2, 7},
+			b:    []int{7, 8, 5, 8, 6, 3, 5},
+			c:    []int{8, 3, 2, 6, 8, 4, 1},
+			want: 46,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			act := Vacation(tt.N, tt.a, tt.b, tt.c)
+			if act != tt.want {
+				t.Errorf("want: %d, act: %d", tt.want, act)
+			}
+		})
+	}
+}
