05378ea58f48d8efe69398f7077d6c7843c77100
rm-ryou <rmco.420118@gmail.com>
2025-12-23 23:40:00 +0900

    feat: add prob

--- books/algorithm_data_structure/chap07/problem/problem.go
+++ books/algorithm_data_structure/chap07/problem/problem.go
@@ -70,3 +70,32 @@ func PlainPoints(n int, a, b []Cod) int {
 
 	return count
 }
+
+func Megalo(n int, a, b []int) bool {
+	flg := true
+
+	dAry := make([][]int, n)
+	for i := range dAry {
+		dAry[i] = make([]int, 2)
+		dAry[i][0] = a[i] // 終えるのに要する時間
+		dAry[i][1] = b[i] // 締め切り
+	}
+
+	slices.SortFunc(dAry, func(a, b []int) int {
+		if a[1] > b[1] {
+			return 1
+		} else {
+			return -1
+		}
+	})
+
+	var finishTime int
+	for _, v := range dAry {
+		finishTime += v[0]
+		if finishTime > v[1] {
+			flg = false
+		}
+	}
+
+	return flg
+}
--- books/algorithm_data_structure/chap07/problem/problem_test.go
+++ books/algorithm_data_structure/chap07/problem/problem_test.go
@@ -125,5 +125,110 @@ func Test_PlainPoints(t *testing.T) {
 			})
 		}
 	})
+}
 
+func Test_Megalo(t *testing.T) {
+	type testCase struct {
+		name string
+		n    int
+		a, b []int
+		want bool
+	}
+
+	testCases := []testCase{
+		{
+			name: "return true",
+			n:    5,
+			a:    []int{2, 1, 1, 4, 3},
+			b:    []int{4, 9, 8, 9, 12},
+			want: true,
+		},
+		{
+			name: "return false",
+			n:    3,
+			a:    []int{334, 334, 334},
+			b:    []int{1000, 1000, 1000},
+			want: false,
+		},
+		{
+			name: "return true",
+			n:    30,
+			a: []int{
+				384,
+				1725,
+				170,
+				4,
+				2,
+				578,
+				702,
+				143,
+				1420,
+				24,
+				849,
+				76,
+				85,
+				444,
+				719,
+				470,
+				1137,
+				455,
+				110,
+				15,
+				368,
+				104,
+				3,
+				366,
+				7,
+				610,
+				152,
+				4,
+				292,
+				334,
+			},
+			b: []int{
+				8895,
+				9791,
+				1024,
+				11105,
+				6,
+				1815,
+				3352,
+				5141,
+				6980,
+				1602,
+				999,
+				7586,
+				5570,
+				4991,
+				11090,
+				10708,
+				4547,
+				9003,
+				9901,
+				8578,
+				3692,
+				1286,
+				4,
+				12143,
+				6649,
+				2374,
+				7324,
+				7042,
+				11386,
+				5720,
+			},
+			want: true,
+		},
+	}
+
+	t.Run("aiの時間を要し、biの時刻が締め切りの中、全てのタスクをこなせられるか", func(t *testing.T) {
+		for _, tc := range testCases {
+			t.Run(tc.name, func(t *testing.T) {
+				act := Megalo(tc.n, tc.a, tc.b)
+				if act != tc.want {
+					t.Errorf("want: %t, act: %t", tc.want, act)
+				}
+			})
+		}
+	})
 }
