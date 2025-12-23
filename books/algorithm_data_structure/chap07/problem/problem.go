2bcc73da46d548e86917527bf73a48442c0fe982
rm-ryou <rmco.420118@gmail.com>
2025-12-23 23:22:15 +0900

    feat: add prob

--- books/algorithm_data_structure/chap07/problem/problem.go
+++ books/algorithm_data_structure/chap07/problem/problem.go
@@ -1,6 +1,7 @@
 package problem
 
 import (
+	"slices"
 	"sort"
 )
 
@@ -13,6 +14,57 @@ func Pair(a, b []int) int {
 	for _, v := range b {
 		if a[count] < v {
 			count++
+		}
+	}
+
+	return count
+}
+
+type Cod struct {
+	X, Y int
+}
+
+func sortCodByX(c []Cod) []Cod {
+	slices.SortFunc(c, func(a, b Cod) int {
+		if a.X > b.X {
+			return 1
+		} else {
+			return -1
+		}
+	})
+
+	return c
+}
+
+func PlainPoints(n int, a, b []Cod) int {
+	var count int
+
+	sortedB := sortCodByX(b)
+	isUsed := make([]bool, n)
+
+	for _, vb := range sortedB {
+
+		hitId := -1
+		maxYOfA := -1
+		for j, v := range a {
+			if isUsed[j] {
+				continue
+			}
+
+			if v.X >= vb.X {
+				continue
+			}
+			if v.Y < maxYOfA {
+				continue
+			}
+
+			maxYOfA = v.Y
+			hitId = j
+		}
+
+		if hitId != -1 {
+			count++
+			isUsed[hitId] = true
 		}
 	}
 
--- books/algorithm_data_structure/chap07/problem/problem_test.go
+++ books/algorithm_data_structure/chap07/problem/problem_test.go
@@ -10,18 +10,18 @@ func Test_Pair(t *testing.T) {
 	}
 
 	testCases := []testCase{
-		// {
-		// 	name: "return 3",
-		// 	a:    []int{1, 2, 3},
-		// 	b:    []int{4, 5, 6},
-		// 	want: 3,
-		// },
-		// {
-		// 	name: "return 0",
-		// 	a:    []int{6, 5, 7},
-		// 	b:    []int{2, 3, 3},
-		// 	want: 0,
-		// },
+		{
+			name: "return 3",
+			a:    []int{1, 2, 3},
+			b:    []int{4, 5, 6},
+			want: 3,
+		},
+		{
+			name: "return 0",
+			a:    []int{6, 5, 7},
+			b:    []int{2, 3, 3},
+			want: 0,
+		},
 		{
 			name: "return 3",
 			a:    []int{8, 1, 2, 7, 5},
@@ -40,4 +40,90 @@ func Test_Pair(t *testing.T) {
 			})
 		}
 	})
+}
+
+func Test_PlainPoints(t *testing.T) {
+	type testCase struct {
+		name string
+		n    int
+		a, b []Cod
+		want int
+	}
+
+	testCases := []testCase{
+		{
+			name: "return 2",
+			n:    3,
+			a: []Cod{
+				{X: 2, Y: 0},
+				{X: 3, Y: 1},
+				{X: 1, Y: 3},
+			},
+			b: []Cod{
+				{X: 4, Y: 2},
+				{X: 0, Y: 4},
+				{X: 5, Y: 5},
+			},
+			want: 2,
+		},
+		{
+			name: "return 2",
+			n:    3,
+			a: []Cod{
+				{X: 0, Y: 0},
+				{X: 1, Y: 1},
+				{X: 5, Y: 2},
+			},
+			b: []Cod{
+				{X: 2, Y: 3},
+				{X: 3, Y: 4},
+				{X: 4, Y: 5},
+			},
+			want: 2,
+		},
+		{
+			name: "return 0",
+			n:    2,
+			a: []Cod{
+				{X: 2, Y: 2},
+				{X: 3, Y: 3},
+			},
+			b: []Cod{
+				{X: 0, Y: 0},
+				{X: 1, Y: 1},
+			},
+			want: 0,
+		},
+		{
+			name: "return 5",
+			n:    5,
+			a: []Cod{
+				{X: 0, Y: 0},
+				{X: 7, Y: 3},
+				{X: 2, Y: 2},
+				{X: 4, Y: 8},
+				{X: 1, Y: 6},
+			},
+			b: []Cod{
+				{X: 8, Y: 5},
+				{X: 6, Y: 9},
+				{X: 5, Y: 4},
+				{X: 9, Y: 1},
+				{X: 3, Y: 7},
+			},
+			want: 5,
+		},
+	}
+
+	t.Run("ax<bx && ay<byなるペアの最大数を返す", func(t *testing.T) {
+		for _, tc := range testCases {
+			t.Run(tc.name, func(t *testing.T) {
+				act := PlainPoints(tc.n, tc.a, tc.b)
+				if act != tc.want {
+					t.Errorf("want: %d, act: %d", tc.want, act)
+				}
+			})
+		}
+	})
+
 }
